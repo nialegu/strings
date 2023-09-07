f9f243cc0efe097010ef6e912a9ec8d2f09654d1
Leonid Gusev <rastrast207@gmail.com>
2023-09-07 16:15:44 +0300

    changed

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -9,26 +9,8 @@
 using namespace std;
 
 int main() {
-	string s1 = "It's allright, handsome Markise.";
-	string s2 = "princess";
-	string s3("Hello, ");
-
-	s1.erase(0, 15); //delete "It's allright"
-	s1.replace(9, 7, s2); //replace "Markise" with "princess"
-	s1.insert(0, s3); //insert "Hello, "
-	s1.erase(s1.size() - 1, 1); // delete "."
-	s1.append(3, '!'); //append "!!!"
-
-	int x = s1.find(" ");
-	int y = s1.find(',');
-	while (x < s1.size()) {
-		s1.replace(x, 1, "/");
-		x = s1.find(" ");
-	}
-	while (y < s1.size()) {
-		s1.replace(y, 1, "%");
-		y = s1.find(',');
-	}
-	cout << "s1: " << s1 << endl;
+	string s1="                    ", s2="cat";
+	s1.insert(12, s2);
+	cout << s1;
 	return 0;
 }
